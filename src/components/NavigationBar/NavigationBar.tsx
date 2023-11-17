ae5b7c55ec04790914608904f017053344b970c6
Leonhard Kenshi Müllauer <52050699+Reonharudo@users.noreply.github.com>
2023-11-17 13:36:42 +0100

    Impl initial about section

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 import { NavigationItem } from "./NavigationItem/NavigationItem";
-import styles from "./NavigationBar.module.css"
+import styles from "./NavigationBar.module.css";
 import { useEffect } from "react";
 
 export function NavigationBar() {
@@ -11,14 +11,15 @@ export function NavigationBar() {
 
     useEffect(() => {
         window.addEventListener("scroll", handleScroll);
-    }, [])
+    }, []);
 
-
-
-    return <nav className={styles.navigation_container}>
-        <NavigationItem url={"#start"} text={"Start"} />
-        <NavigationItem url={"#techstack"} text={"Techstack"} />
-        <NavigationItem url={"#projects"} text={"Projects"} />
-        <NavigationItem url={"#contact"} text={"Contact"} />
-    </nav>
+    return (
+        <nav className={styles.navigation_container}>
+            <NavigationItem url={"#start"} text={"Start"} />
+            <NavigationItem url={"#about"} text={"About"} />
+            <NavigationItem url={"#techstack"} text={"Techstack"} />
+            <NavigationItem url={"#projects"} text={"Projects"} />
+            <NavigationItem url={"#contact"} text={"Contact"} />
+        </nav>
+    );
 }
