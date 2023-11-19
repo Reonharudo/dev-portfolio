577fcd57472c8f868f3e8b9125b8677b0d7bb8a7
Leonhard Kenshi Müllauer <52050699+Reonharudo@users.noreply.github.com>
2023-11-19 17:05:07 +0100

    added some icons to skillitems

--- src/components/TechStackSection/TechStackSection.tsx
+++ src/components/TechStackSection/TechStackSection.tsx
@@ -15,31 +15,110 @@ export function TechStackSection() {
                 />
                 <SkillItem text={"Java Swing"} />
                 <SkillItem text={"JavaFX"} />
-                <SkillItem text={"Spring Boot"} />
+                <SkillItem
+                    text={"Spring Boot"}
+                    image={{
+                        url: "/technology-icons/spring_icon.svg",
+                        altText: "Java",
+                    }}
+                />
                 <SkillItem text={"RestAPI"} />
-                <SkillItem text={"Maven"} />
-                <SkillItem text={"Gradle"} />
+                <SkillItem
+                    text={"Maven"}
+                    image={{
+                        url: "/technology-icons/maven_icon.svg",
+                        altText: "Java",
+                    }}
+                />
+                <SkillItem
+                    text={"Gradle"}
+                    image={{
+                        url: "/technology-icons/gradle_icon.svg",
+                        altText: "Java",
+                    }}
+                />
 
-                <SkillItem text={"MySQL"} />
-                <SkillItem text={"PostgresDB"} />
+                <SkillItem
+                    text={"MySQL"}
+                    image={{
+                        url: "/technology-icons/mysql_icon.svg",
+                        altText: "Java",
+                    }}
+                />
+                <SkillItem
+                    text={"PostgreSQL"}
+                    image={{
+                        url: "/technology-icons/postgresdb_icon.svg",
+                        altText: "Java",
+                    }}
+                />
                 <SkillItem text={"NoSQL CouchDB"} />
-                <SkillItem text={"NoSQL MongoDB"} />
+                <SkillItem
+                    text={"NoSQL MongoDB"}
+                    image={{
+                        url: "/technology-icons/mongodb_icon.svg",
+                        altText: "Java",
+                    }}
+                />
                 <SkillItem text={"Oracle Database PL/SQL"} />
                 <SkillItem text={"S3 Storage"} />
 
-                <SkillItem text={"NodeJS"} />
-                <SkillItem text={"Javascript / Typescript"} />
-                <SkillItem text={"Angular"} />
-                <SkillItem text={"jQuery"} />
+                <SkillItem
+                    text={"NodeJS"}
+                    image={{
+                        url: "/technology-icons/nodejs_icon.svg",
+                        altText: "Java",
+                    }}
+                />
+                <SkillItem
+                    text={"Javascript / Typescript"}
+                    image={{
+                        url: "/technology-icons/typescript_icon.svg",
+                        altText: "Java",
+                    }}
+                />
+                <SkillItem
+                    text={"Angular"}
+                    image={{
+                        url: "/technology-icons/angular_icon.svg",
+                        altText: "Java",
+                    }}
+                />
+                <SkillItem
+                    text={"jQuery"}
+                    image={{
+                        url: "/technology-icons/jquery_icon.svg",
+                        altText: "Java",
+                    }}
+                />
                 <SkillItem text={"Vue"} />
-                <SkillItem text={"PHP"} />
-                <SkillItem text={"NextJS (SSR, ISR, SSG)"} />
+                <SkillItem
+                    text={"PHP"}
+                    image={{
+                        url: "/technology-icons/php_icon.svg",
+                        altText: "Java",
+                    }}
+                />
+                <SkillItem
+                    text={"NextJS (SSR, ISR, SSG)"}
+                    image={{
+                        url: "/technology-icons/nextjs_icon.svg",
+                        altText: "Java",
+                    }}
+                />
                 <SkillItem text={"SharpJS"} />
                 <SkillItem text={"ExpressJS"} />
                 <SkillItem text={"React"} />
                 <SkillItem text={"CSS"} />
                 <SkillItem text={"Bootstrap"} />
+                <SkillItem text={"SauceLabs"} />
+                <SkillItem text={"Mocha"} />
+                <SkillItem text={"Jasmine"} />
+                <SkillItem text={"Sequelize"} />
                 <SkillItem text={"Prisma"} />
+
+                <SkillItem text={"JSON"} />
+                <SkillItem text={"XML"} />
 
                 <SkillItem text={"Micro16"} />
                 <SkillItem text={"Cisco"} />
@@ -49,16 +128,16 @@ export function TechStackSection() {
                 <SkillItem text={"Playwright"} />
                 <SkillItem text={"Cypress"} />
                 <SkillItem text={"Mockito"} />
-                <SkillItem text={"JUNit"} />
+                <SkillItem text={"JUnit"} />
                 <SkillItem text={"TDD"} />
 
                 <SkillItem text={"Haskell"} />
 
                 <SkillItem text={"Github / Gitlab / BitBucket"} />
                 <SkillItem text={"Github Workflow"} />
-                <SkillItem
-                    text={"AWS / DO / GCP (Programmable Search Engine)"}
-                />
+                <SkillItem text={"AWS"} />
+                <SkillItem text={"Digital Ocean"} />
+                <SkillItem text={"GCP (Programmable Search Engine)"} />
                 <SkillItem text={"XMPP"} />
                 <SkillItem text={"Jira"} />
                 <SkillItem text={"SCRUM"} />
@@ -68,9 +147,17 @@ export function TechStackSection() {
                 <SkillItem text={"Jenkins"} />
 
                 <SkillItem text={"Figma"} />
+                <SkillItem text={"UML"} />
+                <SkillItem text={"ERM (Chen Notation)"} />
+
+                <SkillItem text={"Windows"} />
+                <SkillItem text={"MacOS"} />
+                <SkillItem text={"Linux (Ubuntu)"} />
 
                 <SkillItem text={"Python"} />
                 <SkillItem text={"Pandas"} />
+                <SkillItem text={"C#"} />
+                <SkillItem text={"ASP .NET Core"} />
             </div>
         </section>
     );
