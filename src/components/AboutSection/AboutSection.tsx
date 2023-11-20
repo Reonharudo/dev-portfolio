fd248693d65101e28f926e077c5be2efe35a28e0
Leonhard Kenshi Müllauer <52050699+Reonharudo@users.noreply.github.com>
2023-11-20 21:31:04 +0100

    impl about section image gallery

--- src/components/AboutSection/AboutSection.tsx
+++ src/components/AboutSection/AboutSection.tsx
@@ -1,6 +1,59 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
 import styles from "./AboutSection.module.css";
+import { ImageGallery } from "../ImageGallery/ImageGallery";
 
 export function AboutSection() {
+    const [showImageGallery, setShowImageGallery] = useState<boolean>(false);
+
+    const btnRef = useRef<HTMLButtonElement | null>(null);
+    const imageGalleryWrapper = useRef<HTMLDivElement | null>(null);
+
+    function scrollToBtn() {
+        const offset = 100;
+        btnRef.current?.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+            inline: "nearest",
+        });
+
+        window.scrollBy(0, offset);
+    }
+
+    function scrollToImageGallery() {
+        const offset = 100;
+        imageGalleryWrapper.current?.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+            inline: "nearest",
+        });
+
+        window.scrollBy(0, -offset);
+    }
+
+    function calculateAge(birthYear = 2001, birthMonth = 2, birthDay = 5) {
+        const today = new Date();
+        const birthdate = new Date(birthYear, birthMonth - 1, birthDay);
+
+        let age = today.getFullYear() - birthdate.getFullYear();
+
+        if (
+            today.getMonth() < birthdate.getMonth() ||
+            (today.getMonth() === birthdate.getMonth() &&
+                today.getDate() < birthdate.getDate())
+        ) {
+            age--;
+        }
+
+        return age;
+    }
+
+    useEffect(() => {
+        if (showImageGallery) {
+            scrollToImageGallery();
+        }
+    }, [showImageGallery]);
+
     return (
         <section id="about" className={styles.container}>
             <h1>About me</h1>
@@ -8,9 +61,9 @@ export function AboutSection() {
                 <div className={styles.description_container}>
                     <p>Welcome on my page!</p>
                     <p>
-                        I am a 23 year old Software Engineer with a relentless
-                        passion for learning and staying at the forefront of
-                        technology.
+                        I am a {calculateAge()} year old Software Engineer with
+                        a relentless passion for learning and staying at the
+                        forefront of technology.
                     </p>
                     <p>
                         I embrace the philosophy that FOMO(Fear of Missing Out)
@@ -20,8 +73,14 @@ export function AboutSection() {
 
                     <p>
                         Beyond my software engineering endeavours, I am a
-                        hardware enthusiast as you may see on the pictures on
-                        your right :) <br />
+                        hardware enthusiast as you may see on the{" "}
+                        <span
+                            className={styles.inline_btn}
+                            onClick={scrollToBtn}
+                        >
+                            pictures
+                        </span>{" "}
+                        :) <br />
                         Additionally, I actively collect and peruse Wikipedia
                         articles spanning topics from medical science to
                         philosophical or history, maintaining them as valuable
@@ -31,9 +90,40 @@ export function AboutSection() {
                     <p>Leonhard Muellauer</p>
                 </div>
 
-                <div className={styles.image_section}>
-                    <button>Show Image</button>
-                </div>
+                {showImageGallery && (
+                    <div
+                        className={styles.image_gallery_wrapper}
+                        ref={imageGalleryWrapper}
+                    >
+                        <button
+                            className={styles.btn}
+                            onClick={() => setShowImageGallery(false)}
+                        >
+                            Hide
+                        </button>
+                        <ImageGallery
+                            images={[
+                                {
+                                    imageURL:
+                                        "/projects/engaku/add_series_modal.png",
+                                },
+                            ]}
+                            isImmutable={false}
+                        />
+                    </div>
+                )}
+                {!showImageGallery && (
+                    <div className={styles.image_section}>
+                        <button
+                            onClick={() => setShowImageGallery(true)}
+                            ref={btnRef}
+                            className={styles.btn}
+                            id="show_images_about_section"
+                        >
+                            Show Images
+                        </button>
+                    </div>
+                )}
             </div>
         </section>
     );
