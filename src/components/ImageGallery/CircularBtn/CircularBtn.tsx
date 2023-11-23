2fc2129a6c9e17ea027daff01d4c5a3fc28b78fc
Leonhard Kenshi Müllauer <52050699+Reonharudo@users.noreply.github.com>
2023-11-23 22:46:10 +0100

    added dep

--- src/components/ImageGallery/CircularBtn/CircularBtn.tsx
+++ src/components/ImageGallery/CircularBtn/CircularBtn.tsx
@@ -20,7 +20,7 @@ export function CircularBtn({
         if (isActive) {
             handleOnActive(buttonRef.current);
         }
-    }, [isActive]);
+    }, [isActive, handleOnActive]);
 
     return (
         <button
